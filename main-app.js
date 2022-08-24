7b4cd4326f0bf1c28af26111a9017ece8f81c01e
Vit Mur <myp1blka@gmail.com>
2022-08-25 00:27:21 +0300

    lvl2

--- main-app.js
+++ main-app.js
@@ -3,6 +3,8 @@
 const express = require ("express");
 const server = express();
 
+let counterOfRequests = 0;
+
 server.set('view engine', 'ejs');
 server.set('views', './views');
 
@@ -10,9 +12,20 @@ server.use(express.static('./pub'));
 
 // ############################################               Роути              ############################################
 
+server.use((req, res, next) => {
+    counterOfRequests++;
+    console.log(counterOfRequests);
+    next();
+})
+
+server.use('/video/home.mp4', (req, res, next) => {
+    console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+    next();
+})
+
 server.get('/', (req,res) => {
     res.render('main');
-    })
+})
 
 server.get('/123', (req,res) => {
     res.send('123');
@@ -27,11 +40,17 @@ server.get('/products/:id', (req, res) => {
     const { id } = req.params;
     const products = (require("./products"));
 
-    const id_prod = Number(id);
-    const product = products.products_db.find(item => item.id === id_prod);
+    const idProd = Number(id);
+    const product = products.products_db.find(item => item.id === idProd);
 
     res.render('product', { product });
     //res.send('product id is: ' + id);
+})
+
+server.use((req, res, next) => {
+    res.statusCode = 404;
+    res.render('404');
+    next();
 })
 
 
--- pub/js/main.js
+++ pub/js/main.js
@@ -2,33 +2,33 @@
 const preloader_elem = document.getElementById('preloader');
 const products_elem = document.getElementById('products-div');
 
-const GetOverHere = async () => {
+const getOverHere = async () => {
         // отримати дані від віддаленого сервера
         const resultProductsObj = await axios.get('./products/');
 
         // відмалювати отримані дані в HTML (передаємо об'єкт)
-        DrawProducts(resultProductsObj);
+        drawProducts(resultProductsObj);
 }
 
-const DrawProducts = (result) => {
+const drawProducts = (result) => {
     let productParams = ""; // очистити накопичувальну HTML змінну
-    let main_key ;
+    let mainKey ;
 
-    for (main_key in result.data) { // перелічити всі параметри в об'єкті та їх значення
-        console.log('key = ' + result.data[main_key]);
+    for (mainKey in result.data) { // перелічити всі параметри в об'єкті та їх значення
+        console.log('key = ' + result.data[mainKey]);
     }
 
-    for (let i in result.data[main_key]) {
-        productParams += `<div class="product"><a href="/products/${result.data[main_key][i]['id']}">
-        <img height="150px" width="150px" src="${result.data[main_key][i]['picture']}"><br>
-        <b>${result.data[main_key][i]['title']}</b><br>${result.data[main_key][i]['price']} грн.</a></div><br>`; // форматуємо виводимі дані
+    for (let i in result.data[mainKey]) {
+        productParams += `<div class="product"><a href="/products/${result.data[mainKey][i]['id']}">
+        <img height="150px" width="150px" src="${result.data[mainKey][i]['picture']}"><br>
+        <b>${result.data[mainKey][i]['title']}</b><br>${result.data[mainKey][i]['price']} грн.</a></div><br>`; // форматуємо виводимі дані
     }
 
     products_elem.insertAdjacentHTML('beforeend', productParams); // виводимо всі дані накопичені у змінній
     preloader_elem.style.display = 'none';
 }
 
-GetOverHere();
+getOverHere();
 
 
 
